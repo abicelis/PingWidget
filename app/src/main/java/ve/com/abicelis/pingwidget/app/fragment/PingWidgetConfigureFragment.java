87c77b9bfc94335c40226f90f418c0330b42878e
abicelis <abicelis@gmail.com>
2017-02-12 18:12:31 -0400

    Bugfix mAddress.setOnPreferenceChangeListener() held old value Also added about section: About, version, rate app..

--- app/src/main/java/ve/com/abicelis/pingwidget/app/fragment/PingWidgetConfigureFragment.java
+++ app/src/main/java/ve/com/abicelis/pingwidget/app/fragment/PingWidgetConfigureFragment.java
@@ -1,12 +1,19 @@
 package ve.com.abicelis.pingwidget.app.fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.util.Pair;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.widget.RemoteViews;
@@ -15,6 +22,8 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.color.ColorChooserDialog;
 import com.takisoft.fix.support.v7.preference.EditTextPreference;
 import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
+import java.util.Locale;
 
 import ve.com.abicelis.pingwidget.R;
 import ve.com.abicelis.pingwidget.app.activity.PingWidgetConfigureActivity;
@@ -30,12 +39,21 @@ import ve.com.abicelis.pingwidget.util.SharedPreferencesHelper;
 
 public class PingWidgetConfigureFragment extends PreferenceFragmentCompat {
 
+    //CONST
+    public static final String GITHUB_URL = "github.com/abicelis/";
+    public static final String WEBSITE_URL = "www.alejandrobicelis.com.ve";
+
+    //UI
     private EditTextPreference mAddress;
     private ListPreference mInterval;
+    private ListPreference mMaxPings;
     private Preference mBackgroundColor;
     private Preference mChartLineColor;
-    private ListPreference mMaxPings;
+    private Preference mAbout;
+    private Preference mRate;
+    private Preference mContact;
 
+    //DATA
     private int mWidgetBackgroundColor = -1;
     private int mWidgetChartLineColor = -1;
 
@@ -43,34 +61,36 @@ public class PingWidgetConfigureFragment extends PreferenceFragmentCompat {
     public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.ping_widget_configure_preferences);
 
-        mAddress = (EditTextPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_key_address));
+        mAddress = (EditTextPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_address_key));
         mAddress.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                //Toast.makeText(getActivity(), "TODO: Check if valid IP", Toast.LENGTH_SHORT).show();
-                //TODO: Check if valid IP
+                mAddress.setSummary((String) newValue);
                 return true;
             }
         });
-        mInterval = (ListPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_key_interval));
-        mBackgroundColor =  findPreference(getResources().getString(R.string.fragment_widget_configure_key_background_color));
-        mBackgroundColor.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
+        mInterval = (ListPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_interval_key));
+        mMaxPings = (ListPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_max_pings_key));
 
-                new ColorChooserDialog.Builder((PingWidgetConfigureActivity) getActivity(), R.string.fragment_widget_configure_dialog_background_color)
-                        .tag(getResources().getString(R.string.fragment_widget_configure_dialog_background_color))
-                        .accentMode(true)
-                        .doneButton(R.string.md_done_label)  // changes label of the done button
-                        .cancelButton(R.string.md_cancel_label)  // changes label of the cancel button
-                        .backButton(R.string.md_back_label)  // changes label of the back button
-                        .dynamicButtonColor(true)  // defaults to true, false will disable changing action buttons' color to currently selected color
-                        .show();
-                return false;
-            }
-        });
+//        mBackgroundColor =  findPreference(getResources().getString(R.string.fragment_widget_configure_background_color_key));
+//        mBackgroundColor.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            @Override
+//            public boolean onPreferenceClick(Preference preference) {
+//
+//                new ColorChooserDialog.Builder((PingWidgetConfigureActivity) getActivity(), R.string.fragment_widget_configure_dialog_background_color)
+//                        .tag(getResources().getString(R.string.fragment_widget_configure_dialog_background_color))
+//                        .accentMode(true)
+//                        .doneButton(R.string.md_done_label)  // changes label of the done button
+//                        .cancelButton(R.string.md_cancel_label)  // changes label of the cancel button
+//                        .backButton(R.string.md_back_label)  // changes label of the back button
+//                        .dynamicButtonColor(true)  // defaults to true, false will disable changing action buttons' color to currently selected color
+//                        .show();
+//                return false;
+//            }
+//        });
         mWidgetBackgroundColor = Color.parseColor("#448AFF");
-        mChartLineColor =  findPreference(getResources().getString(R.string.fragment_widget_configure_key_line_color));
+
+        mChartLineColor =  findPreference(getResources().getString(R.string.fragment_widget_configure_line_color_key));
         mChartLineColor.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
@@ -87,75 +107,63 @@ public class PingWidgetConfigureFragment extends PreferenceFragmentCompat {
             }
         });
         mWidgetChartLineColor = Color.parseColor("#FFEB3B");
-        mMaxPings = (ListPreference) findPreference(getResources().getString(R.string.fragment_widget_configure_key_max_pings));
 
-
-        Preference enable = findPreference(getResources().getString(R.string.fragment_widget_configure_key_enable));
-        enable.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        mAbout = findPreference(getResources().getString(R.string.fragment_widget_configure_about_key));
+        mAbout.setSummary(String.format(Locale.getDefault(), getResources().getString(R.string.fragment_widget_configure_about_summary), getAppVersionAndBuild(getActivity()).first));
+        mRate = findPreference(getResources().getString(R.string.fragment_widget_configure_rate_key));
+        mRate.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-
-                if(!checkValues()) {
-                    return true;
-                }
-
-                Bundle extras = getArguments();
-                if (extras != null) {
-
-                    //Get the arguments (bundle) passed from PingWidgetConfigureActivity, which contains the widgetId
-                    int widgetId = getArguments().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
-                    //Get AppWidgetManager and RemoteViews
-                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getActivity());
-                    RemoteViews views = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
-
-                    //Update the widget's views
-                    views.setTextViewText(R.id.widget_host, mAddress.getText());
-                    views.setImageViewResource(R.id.widget_start_pause, android.R.drawable.ic_media_play);
-
-                    //TODO: Cant seem to figure out how to change background color without overwriting background drawable (killing the rounded corners)
-                    //Do this here and in PingWidgetProvider
-                    //views.setInt(R.id.widget_background, "setColorFilter", Color.WHITE);
-                    //views.setInt(R.id.widget_background, "setBackgroundTint", Color.GREEN);
-                     views.setInt(R.id.widget_background, "setBackgroundColor", mWidgetBackgroundColor);
-                    //views.setInt(R.id.widget_layout, "setBackgroundColor", mWidgetBackgroundColor);
-                    //And if your color has transparency call remoteViews.setInt(R.id.backgroundimage, "setImageAlpha", Color.alpha(color); if SDK>=16; else remoteViews.setInt(R.id.backgroundimage, "setAlpha", Color.alpha(color);
-
-                    //Save PingWidgetData in SharedPreferences()
-                    savePingWidgetData(widgetId, mAddress.getText(), Integer.parseInt(mInterval.getValue()), Integer.parseInt(mMaxPings.getValue()));
-
-
-                    //Register an Intent so that onClicks on the widget are received by PingWidgetProvider.onReceive()
-                    //Create an Intent, set PING_WIDGET_TOGGLE action to it, put EXTRA_APPWIDGET_ID as extra
-                    Intent clickIntent = new Intent(getActivity(), PingWidgetProvider.class);
-                    clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
-                    clickIntent.setAction(PingWidgetProvider.PING_WIDGET_TOGGLE);
-
-                    //Construct a PendingIntent using the Intent above
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), widgetId, clickIntent, 0);
-
-                    //Register pendingIntent in RemoteViews onClick
-                    views.setOnClickPendingIntent(R.id.widget_start_pause, pendingIntent);
-
-
-
-                    //Finally, update the widget
-                    appWidgetManager.updateAppWidget(widgetId, views);
-
-                    //Send widget an intent with RESULT_OK so it know its been configured correctly
-                    Intent resultValue = new Intent();
-                    resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
-                    getActivity().setResult(Activity.RESULT_OK, resultValue);
-                    getActivity().finish();
-
-                } else {
-                    Toast.makeText(getActivity(), "Error! No WidgetID passed to Activity", Toast.LENGTH_SHORT).show();
-                }
-
-                //Return true, click was handled here
+                Intent playStoreIntent = new Intent(Intent.ACTION_VIEW);
+                playStoreIntent.setData(Uri.parse(getResources().getString(R.string.url_market)));
+                startActivity(playStoreIntent);
                 return true;
             }
         });
+        mContact = findPreference(getResources().getString(R.string.fragment_widget_configure_contact_key));
+        mContact.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",getResources().getString(R.string.address_email), null));
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                return true;
+            }
+        });
+
+    }
+
+    private static Pair<String, Integer> getAppVersionAndBuild(Context context) {
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return new Pair<>(pInfo.versionName, pInfo.versionCode);
+        } catch (Exception e) {
+            return new Pair<>("", 0);
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    private static boolean launchWebBrowser(Context context, String url) {
+        try {
+            url = url.toLowerCase();
+            if (!url.startsWith("http://") || !url.startsWith("https://")) {
+                url = "http://" + url;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            ResolveInfo resolveInfo = context.getPackageManager().resolveActivity(intent,
+                    PackageManager.MATCH_DEFAULT_ONLY);
+            if (null == resolveInfo) {
+                Toast.makeText(context, "Could not find a Browser to open link", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            Toast.makeText(context, "Could not start web browser", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
     }
 
     private boolean checkValues() {
@@ -192,10 +200,77 @@ public class PingWidgetConfigureFragment extends PreferenceFragmentCompat {
         SharedPreferencesHelper.writePingWidgetData(getContext().getApplicationContext(), widgetId, data);
     }
 
-    //Method called from parent activity
+
+
+
+    //Methods called from parent activity
+    public void handleWidgetCreation() {
+
+        if(!checkValues()) {
+            return;
+        }
+
+        Bundle extras = getArguments();
+        if (extras != null) {
+
+            //Get the arguments (bundle) passed from PingWidgetConfigureActivity, which contains the widgetId
+            int widgetId = getArguments().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+            //Get AppWidgetManager and RemoteViews
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getActivity());
+            RemoteViews views = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
+
+            //Update the widget's views
+            views.setTextViewText(R.id.widget_host, mAddress.getText());
+            views.setImageViewResource(R.id.widget_start_pause, android.R.drawable.ic_media_play);
+
+            //TODO: switch here when ThemePreference is done, selecting widget_layout_container_top background
+            //Do this here and in PingWidgetProvider
+            //views.setInt(R.id.widget_background, "setColorFilter", Color.WHITE);
+            //views.setInt(R.id.widget_background, "setBackgroundTint", Color.GREEN);
+            //views.setInt(R.id.widget_background, "setBackgroundColor", mWidgetBackgroundColor);
+            //views.setInt(R.id.widget_layout, "setBackgroundColor", mWidgetBackgroundColor);
+            //And if your color has transparency call remoteViews.setInt(R.id.backgroundimage, "setImageAlpha", Color.alpha(color); if SDK>=16; else remoteViews.setInt(R.id.backgroundimage, "setAlpha", Color.alpha(color);
+
+            //Save PingWidgetData in SharedPreferences()
+            savePingWidgetData(widgetId, mAddress.getText(), Integer.parseInt(mInterval.getValue()), Integer.parseInt(mMaxPings.getValue()));
+
+
+            //Register an Intent so that onClicks on the widget are received by PingWidgetProvider.onReceive()
+            //Create an Intent, set PING_WIDGET_TOGGLE action to it, put EXTRA_APPWIDGET_ID as extra
+            Intent clickIntent = new Intent(getActivity(), PingWidgetProvider.class);
+            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+            clickIntent.setAction(PingWidgetProvider.PING_WIDGET_TOGGLE);
+
+            //Construct a PendingIntent using the Intent above
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), widgetId, clickIntent, 0);
+
+            //Register pendingIntent in RemoteViews onClick
+            views.setOnClickPendingIntent(R.id.widget_start_pause, pendingIntent);
+
+
+
+            //Finally, update the widget
+            appWidgetManager.updateAppWidget(widgetId, views);
+
+            //Send widget an intent with RESULT_OK so it know its been configured correctly
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+            getActivity().setResult(Activity.RESULT_OK, resultValue);
+            getActivity().finish();
+
+        } else {
+            Toast.makeText(getActivity(), "Error! No WidgetID passed to Activity", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
     public void setWidgetBackgroundColor(int color) {
         mWidgetBackgroundColor = color;
     }
+
     public void setWidgetChartLineColor(int color) {
         mWidgetChartLineColor = color;
     }
