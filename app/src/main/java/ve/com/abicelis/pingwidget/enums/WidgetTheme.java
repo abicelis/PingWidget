f6cae9934bf8cc6e5b4860d61623fec4a99107a4
abicelis <abicelis@gmail.com>
2017-05-11 16:24:36 -0400

    Update: Added support for short version of widget layout

--- app/src/main/java/ve/com/abicelis/pingwidget/enums/WidgetTheme.java
+++ app/src/main/java/ve/com/abicelis/pingwidget/enums/WidgetTheme.java
@@ -12,24 +12,26 @@ import ve.com.abicelis.pingwidget.R;
  */
 
 public enum WidgetTheme {
-    BLUE_YELLOW(R.id.theme_blue_yellow, R.string.widget_theme_summary_blue_yellow, R.drawable.widget_background_top_blue, R.color.widget_theme_blue_yellow_background_top, R.color.widget_theme_blue_yellow_chart),
-    GREEN_GREEN(R.id.theme_green_green, R.string.widget_theme_summary_green_green, R.drawable.widget_background_top_green, R.color.widget_theme_green_background_top, R.color.widget_theme_green_chart),
-    ORANGE_AMBER(R.id.theme_orange_amber, R.string.widget_theme_summary_orange_amber, R.drawable.widget_background_top_orange, R.color.widget_theme_orange_amber_background_top, R.color.widget_theme_orange_amber_chart),
-    RED_TEAL(R.id.theme_red_teal, R.string.widget_theme_summary_red_teal, R.drawable.widget_background_top_red, R.color.widget_theme_red_teal_background_top, R.color.widget_theme_red_teal_chart),
-    TEAL_ORANGE(R.id.theme_teal_orange, R.string.widget_theme_summary_teal_orange, R.drawable.widget_background_top_teal, R.color.widget_theme_teal_orange_background_top, R.color.widget_theme_teal_orange_chart),
-    PURPLE_ORANGE(R.id.theme_purple_orange, R.string.widget_theme_summary_purple_orange, R.drawable.widget_background_top_purple, R.color.widget_theme_purple_orange_background_top, R.color.widget_theme_purple_orange_chart)
+    BLUE_YELLOW(R.id.theme_blue_yellow,     R.string.widget_theme_summary_blue_yellow,      R.drawable.widget_background_top_blue_tall,     R.drawable.widget_background_top_blue_short,    R.color.widget_theme_blue_yellow_background_top,    R.color.widget_theme_blue_yellow_chart),
+    GREEN_GREEN(R.id.theme_green_green,     R.string.widget_theme_summary_green_green,      R.drawable.widget_background_top_green_tall,    R.drawable.widget_background_top_green_short,   R.color.widget_theme_green_background_top,          R.color.widget_theme_green_chart),
+    ORANGE_AMBER(R.id.theme_orange_amber,   R.string.widget_theme_summary_orange_amber,     R.drawable.widget_background_top_orange_tall,   R.drawable.widget_background_top_orange_short,  R.color.widget_theme_orange_amber_background_top,   R.color.widget_theme_orange_amber_chart),
+    RED_TEAL(R.id.theme_red_teal,           R.string.widget_theme_summary_red_teal,         R.drawable.widget_background_top_red_tall,      R.drawable.widget_background_top_red_short,     R.color.widget_theme_red_teal_background_top,       R.color.widget_theme_red_teal_chart),
+    TEAL_ORANGE(R.id.theme_teal_orange,     R.string.widget_theme_summary_teal_orange,      R.drawable.widget_background_top_teal_tall,     R.drawable.widget_background_top_teal_short,    R.color.widget_theme_teal_orange_background_top,    R.color.widget_theme_teal_orange_chart),
+    PURPLE_ORANGE(R.id.theme_purple_orange, R.string.widget_theme_summary_purple_orange,    R.drawable.widget_background_top_purple_tall,   R.drawable.widget_background_top_purple_short,  R.color.widget_theme_purple_orange_background_top,  R.color.widget_theme_purple_orange_chart)
     ;
 
     private @IdRes int mThemeViewId;
     private @StringRes int mSummary;
-    private @DrawableRes int mDrawableBackgroundContainerTop;
+    private @DrawableRes int mDrawableBackgroundContainerTopTall;
+    private @DrawableRes int mDrawableBackgroundContainerTopShort;
     private @ColorRes int mColorBackgroundContainerTop;
     private @ColorRes int mColorChart;
 
-    WidgetTheme(@IdRes int themeViewId, @StringRes int summary, @DrawableRes int drawableBackgroundContainerTop, @ColorRes int colorBackgroundContainerTop, @ColorRes int colorChart) {
+    WidgetTheme(@IdRes int themeViewId, @StringRes int summary, @DrawableRes int drawableBackgroundContainerTopTall, @DrawableRes int drawableBackgroundContainerTopShort, @ColorRes int colorBackgroundContainerTop, @ColorRes int colorChart) {
         mThemeViewId = themeViewId;
         mSummary = summary;
-        mDrawableBackgroundContainerTop = drawableBackgroundContainerTop;
+        mDrawableBackgroundContainerTopTall = drawableBackgroundContainerTopTall;
+        mDrawableBackgroundContainerTopShort = drawableBackgroundContainerTopShort;
         mColorBackgroundContainerTop = colorBackgroundContainerTop;
         mColorChart = colorChart;
     }
@@ -42,8 +44,8 @@ public enum WidgetTheme {
         return mThemeViewId;
     }
 
-    public int getDrawableBackgroundContainerTop() {
-        return mDrawableBackgroundContainerTop;
+    public int getDrawableBackgroundContainerTop(WidgetLayoutType widgetLayoutType) {
+        return (widgetLayoutType.equals(WidgetLayoutType.SHORT) ? mDrawableBackgroundContainerTopShort : mDrawableBackgroundContainerTopTall);
     }
 
     public int getColorBackgroundContainerTop() {
