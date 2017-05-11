c212deca7559502cb6011b2f9513094523d19d06
abicelis <abicelis@gmail.com>
2017-05-11 16:24:12 -0400

    New: WidgetLayoutType, added short version of widget layout

---
+++ app/src/main/java/ve/com/abicelis/pingwidget/enums/WidgetLayoutType.java
@@ -0,0 +1,38 @@
+package ve.com.abicelis.pingwidget.enums;
+
+import android.support.annotation.LayoutRes;
+
+import ve.com.abicelis.pingwidget.R;
+
+/**
+ * Created by abice on 11/5/2017.
+ */
+
+public enum WidgetLayoutType {
+    SHORT(R.layout.widget_layout_short),
+    TALL(R.layout.widget_layout_tall);
+
+    private @LayoutRes int mLayout;
+    private static final int WIDGET_LAYOUT_HEIGHT_THRESHOLD = 100;
+
+
+    WidgetLayoutType(@LayoutRes int layout) {
+        mLayout = layout;
+    }
+
+    public int getLayout() {
+        return mLayout;
+    }
+
+
+
+    public static WidgetLayoutType getWidgetLayoutTypeByHeight(int minHeight) {
+        if(minHeight < WIDGET_LAYOUT_HEIGHT_THRESHOLD) {
+            //Use Short view
+            return WidgetLayoutType.SHORT;
+        } else {
+            //Use Tall view
+            return WidgetLayoutType.TALL;
+        }
+    }
+}
