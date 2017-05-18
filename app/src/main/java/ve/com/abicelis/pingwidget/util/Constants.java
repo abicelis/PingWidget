aa82444b673e9261d813e5cceda2bfb1f0a6d9a2
abicelis <abicelis@gmail.com>
2017-05-17 20:22:37 -0400

    Constants refactor

---
+++ app/src/main/java/ve/com/abicelis/pingwidget/util/Constants.java
@@ -0,0 +1,14 @@
+package ve.com.abicelis.pingwidget.util;
+
+/**
+ * Created by abice on 16/5/2017.
+ */
+
+public class Constants {
+
+    public static final String ACTION = "ve.com.abicelis.action.ACTION";
+    public static final String PING_WIDGET_TOGGLE = "ve.com.abicelis.action.PING_WIDGET_TOGGLE";
+    public static final String ACTION_WIDGET_RECONFIGURE = "ve.com.abicelis.action.ACTION_WIDGET_RECONFIGURE";
+    public static final String ACTION_WIDGET_CONFIGURE = "android.appwidget.action.APPWIDGET_CONFIGURE";
+
+}
